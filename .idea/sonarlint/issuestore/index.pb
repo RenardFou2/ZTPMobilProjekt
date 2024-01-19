7cc48c097c9502f5bb4d61b937b7961eaaee06c2
Renard <harpi473@Gmail.com>
2024-01-19 14:16:39 +0100

    Pierwszy widok

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -15,3 +15,7 @@ S
 #.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
 ]
 -.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+s
+Capp/src/main/java/com/example/ztpmobilprojekt/WordListActivity.java,b\4\b41093bd6e41252f3ff233a081b61908b17d8cf8
--- app/src/main/java/com/example/ztpmobilprojekt/MainActivity.java
+++ app/src/main/java/com/example/ztpmobilprojekt/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.ztpmobilprojekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button wordListButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wordListButton = findViewById(R.id.wordListButton);
+
+        wordListButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, WordListActivity.class);
+            startActivity(intent);
+        });
     }
 }
