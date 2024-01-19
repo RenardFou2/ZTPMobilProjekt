dfdbb34a26071f83fb6db52400886ecf85499a0e
eldawidos1111 <eldawidos111@gmail.com>
2024-01-19 17:17:11 +0100

    gitignore

---
+++ app/src/main/java/com/example/ztpmobilprojekt/FillLevelBuilder.java
@@ -0,0 +1,24 @@
+package com.example.ztpmobilprojekt;
+
+import static com.example.ztpmobilprojekt.WordDatabase.getDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FillLevelBuilder implements ILevelBuilder {
+    List<Pair> result = new ArrayList<Pair>();
+
+    @Override
+    public String createAnswer(String language2) {
+        return null;
+    }
+    @Override
+    public String createQuestion(Difficulty difficulty, String language1) {
+        return null;
+    }
+
+    @Override
+    public List<Pair> getList() {
+        return null;
+    }
+}
--- app/src/main/java/com/example/ztpmobilprojekt/ILevelBuilder.java
+++ app/src/main/java/com/example/ztpmobilprojekt/ILevelBuilder.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface ILevelBuilder
 {
-public String createQuestion(Difficulty difficulty, String language1);
+    public String createQuestion(Difficulty difficulty, String language1);
 public String createAnswer(String language2);
 // public Pair createPair(String question, String answer);
 public List<Pair> getList();
