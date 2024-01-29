dd6a209dce43d05aec3dd39f4634e1e7827967ad
remkeah <gmasiewicz@gmail.com>
2024-01-29 07:15:04 +0100

    sensor lokalizacja

--- app/src/main/java/com/example/ztpmobilprojekt/LuckyWordActivity.java
+++ app/src/main/java/com/example/ztpmobilprojekt/LuckyWordActivity.java
@@ -1,14 +1,22 @@
 package com.example.ztpmobilprojekt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.Manifest;
 
 import java.util.List;
 
@@ -17,6 +25,9 @@ public class LuckyWordActivity extends AppCompatActivity {
     private TextView luckyWordLearn;
     private TextView luckyWordMy;
     private TextView luckyWordTextView;
+    private TextView latitudeTextView;
+    private TextView longitudeTextView;
+    private LocationManager locationManager;
     private List<Word> words;
     private WordRepository wordRepository;
     SensorManager sensorManager;
@@ -29,12 +40,26 @@ public class LuckyWordActivity extends AppCompatActivity {
         luckyWordLearn = findViewById(R.id.luckyWordLearnTextView);
         luckyWordMy = findViewById(R.id.luckyWordMyTextView);
         luckyWordTextView = findViewById(R.id.luckyWordTextView);
+        latitudeTextView = findViewById(R.id.latitudeTextView);
+        longitudeTextView = findViewById(R.id.longitudeTextView);
 
         wordRepository = new WordRepository(getApplication());
         words = wordRepository.findWordsByDifficulty(SettingsUtil.getDifficulty().ordinal());
 
-
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if(ContextCompat.checkSelfPermission(LuckyWordActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(LuckyWordActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(LuckyWordActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},1 );
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 1, new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                longitudeTextView.setText(String.valueOf(location.getLongitude()));
+                latitudeTextView.setText(String.valueOf(location.getLatitude()));
+            }
+        });
 
         Sensor sensorShake = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
